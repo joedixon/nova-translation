d5bf9f35e3f423f758dfc46d52d346db19547efc
Christian Widlund <christian.widlund@gmail.com>
2022-09-24 17:03:24 +0200

    require default ...

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,25 +1,27 @@
-Nova.booting((Vue, router) => {
-    router.addRoutes([
+Nova.booting((Vue, router, store) => {
+  Vue.config.devtools = process.env.NODE_ENV === 'development'
+
+  router.addRoutes([
         {
             name: 'nova-translation',
             path: '/nova-translation',
-            component: require('./views/LanguagesIndex'),
+            component: require('./views/LanguagesIndex').default,
         },
         {
             name: 'nova-translation.languages.create',
             path: '/nova-translation/languages/create',
-            component: require('./views/LanguagesCreate'),
+            component: require('./views/LanguagesCreate').default,
         },
         {
             name: 'nova-translation.languages.translations.index',
             path: '/nova-translation/languages/translations',
-            component: require('./views/TranslationsIndex'),
+            component: require('./views/TranslationsIndex').default,
             props: true
         },
         {
             name: 'nova-translation.languages.translations.create',
             path: '/nova-translation/languages/:language/translations/create',
-            component: require('./views/TranslationsCreate'),
+            component: require('./views/TranslationsCreate').default,
             props: true
         },
     ])
