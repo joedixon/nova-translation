8ffb4722a802549e368c07fe0ffd67d0865df626
tom <tom@razorcreations.com>
2023-12-07 17:38:28 +0000

    wip

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,33 +1,14 @@
-// Nova.booting((Vue, router) => {
-//     router.addRoutes([
-//         {
-//             name: 'nova-translation',
-//             path: '/nova-translation',
-//             component: require('./views/LanguagesIndex'),
-//         },
-//         {
-//             name: 'nova-translation.languages.create',
-//             path: '/nova-translation/languages/create',
-//             component: require('./views/LanguagesCreate'),
-//         },
-//         {
-//             name: 'nova-translation.languages.translations.index',
-//             path: '/nova-translation/languages/translations',
-//             component: require('./views/TranslationsIndex'),
-//             props: true
-//         },
-//         {
-//             name: 'nova-translation.languages.translations.create',
-//             path: '/nova-translation/languages/:language/translations/create',
-//             component: require('./views/TranslationsCreate'),
-//             props: true
-//         },
-//     ])
-// })
+// Nova.booting((Vue) => {
+//     window.Vue.use = Vue.use;
+//     Vue.inertia("NovaTranslation", require("./views/LanguagesIndex").default);
+//     Vue.inertia("NovaTranslationLangCreate", require("./views/LanguagesCreate").default);
+//     Vue.inertia("NovaTranslationTranslationIndex", require("./views/TranslationsIndex").default);
+//     Vue.inertia("NovaTranslationTranslationsCreate", require("./views/TranslationsCreate").default);
+// });
 
-Nova.booting((Vue) => {
-    Nova.inertia("NovaTranslation", require("./views/LanguagesIndex").default);
-    Nova.inertia("NovaTranslationLangCreate", require("./views/LanguagesCreate").default);
-    Nova.inertia("NovaTranslationTranslationIndex", require("./views/TranslationsIndex").default);
-    Nova.inertia("NovaTranslationTranslationsCreate", require("./views/TranslationsCreate").default);
-});
+import LanguageIndex from './views/LanguagesIndex'
+
+Nova.booting((app, store) => {
+  Nova.inertia('Test', Tool)
+  Nova.inertia('LanguageIndex', LanguageIndex)
+})
--- webpack.mix.js
+++ webpack.mix.js
@@ -7,5 +7,4 @@ mix
   .setPublicPath('dist')
   .js('resources/js/tool.js', 'js')
   .vue({ version: 3 })
-  .css('resources/css/tool.css', 'css')
   .nova('razorcreations/nova-translation')
